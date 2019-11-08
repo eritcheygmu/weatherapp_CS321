1a0d82399f9385a83e37595c8d08316ead75df07
eritcheygmu <55561283+eritcheygmu@users.noreply.github.com>
2019-11-08 16:48:39 -0500

    Can this now commit from android? I already did via cmd.

---
+++ app/src/main/java/com/example/weatherapp_cs321/SuggestionCalculation.java
@@ -0,0 +1,13 @@
+package com.example.weatherapp_cs321;
+
+/**
+ * The main function of the app. Retrieves temperature and additional data
+ * to output a clothing suggestion.
+ *
+ * @author Evan Ritchey
+ * @since 2019-11-08
+ * @version 1.0
+ */
+public class SuggestionCalculation {
+
+}
